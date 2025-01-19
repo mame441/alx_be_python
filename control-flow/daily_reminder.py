1a7027c1e9c20366182e499d2d92058eb0459470
mame441 <mohammedahmedhajii@gmail.com>
2025-01-19 23:54:24 +0300

    daily remainder updated trice

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -4,16 +4,16 @@ time_bound = input('Is it time-bound? (yes/no): ')
 match priority:
   case "high":
     if time_bound == "yes":
-      print(f"Remainder: '{task}' is a high priority task that requires immediate attention today!")
+      print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
     else:
       print(f"Note: '{task}' is high priority task you should do it as soon as possible")
   case "medium":
     if time_bound == "yes":
-      print(f"Remainder: '{task}' is medium priority task. you have to do it befor the deadline after you finished your high priority tasks.")
+      print(f"Reminder: '{task}' is medium priority task. you have to do it befor the deadline after you finished your high priority tasks.")
     else:
       print(f"Note: '{task}' is medium priority task consider doing it when you are free")
   case "low":
       if time_bound == "no":
         print(f"Note: '{task}' is a low priority task. Consider completing it when you have free time.")
       else:
-        print("Remainder: You have to finish it before the time finishes")
+        print("Reminder: You have to finish it before the time finishes")
