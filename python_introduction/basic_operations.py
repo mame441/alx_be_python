1a2511c01a72760b19b8948b2a1dd8b6131068d8
mame441 <mohammedahmedhajii@gmail.com>
2025-01-19 23:19:37 +0300

    New update

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+subtract = number1 - number2
+product = number1 * number2
+print("Addition of ", number1, "and", number2, "is", sum)
+print( "Subtraction of ", number1, "and", number2, "is", subtract)
+print( "Multiplication of ", number1, "and", number2, "is", product)
